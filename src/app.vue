4022f7d1db55b9dc97626e7e161435f18ab4933c
da_tang <townechow@outlook.com>
2020-09-05 19:02:54 +0800

    fix:自定义icon

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+import selfIco from './assets/xiao_feiji.ico';
+export default {
+  name: "App",
+  methods: {
+    init(){
+      console.log("app 初始化...");
+      document.querySelector("#self_icon").href = selfIco;
+    }
+  },
+  mounted() {
+    this.init();
+  },
+};
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    height: 100%;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
 </style>
